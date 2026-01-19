5de3209781c3b415e51f514227b32db5449a350c
OmarHossam50 <omarhossam50@proton.me>
2026-01-19 23:42:45 +0200

    fix: collisions, replace normal sizes with rects instead, fix many issues and volia

--- game.py
+++ game.py
@@ -14,10 +14,10 @@ BG_COLOR = (32, 125, 195)
 CAR_COLOR = (255, 0, 0)
 
 # Car Settings
-car_width, car_height = 80, 60
-car_x = SCREEN_WIDTH // 2 - car_width // 2
-car_y = SCREEN_HEIGHT - car_height - 10
+CAR_SIZE = (75, 60)
+OBSTACLE_SIZE = (75, 60)
 car_speed = 11
+obstacle_speed = 11
 
 # Road
 road_width = 200
@@ -29,22 +29,31 @@ road_y = 0
 # road center line
 road_line_width = 7
 road_line_x = (road_left + road_right) // 2
-# road_line_height = SCREEN_HEIGHT
-
-# Obstacles (other cars)
-obstacle_width, obstacle_height = 65, 60
-obstacle_x = randint(road_left, road_right - obstacle_width)
-obstacle_y = -obstacle_height
-obstacle_speed = 11
-
-score = 0
 
 ## load images
 car_img = pygame.image.load("car.png")
 obstacle_img = pygame.image.load("obstacle.png")
-car_img = pygame.transform.scale(car_img, (car_width, car_height))
-obstacle_img = pygame.transform.scale(obstacle_img, (obstacle_width, obstacle_height))
 
+# Scale images 
+car_img = pygame.transform.scale(car_img, CAR_SIZE)
+obstacle_img = pygame.transform.scale(obstacle_img, OBSTACLE_SIZE)
+
+# NOW get rects that match the images
+car_rect = car_img.get_rect()
+obstacle_rect = obstacle_img.get_rect()
+car_rect.inflate_ip(-20, -10)
+obstacle_rect.inflate_ip(-20, -10)
+
+# Position cars properly
+car_rect.centerx = SCREEN_WIDTH // 2
+car_rect.bottom = SCREEN_HEIGHT - 20
+
+obstacle_rect.x = randint(road_left, road_right - obstacle_rect.width)
+obstacle_rect.y = -obstacle_rect.height
+
+
+# others
+score = 0
 font = pygame.font.SysFont(None, 36)
 
 # Main game loop
@@ -55,20 +64,19 @@ while running:
     
     score += 0.1
     
-    obstacle_y += obstacle_speed
-    if obstacle_y > SCREEN_HEIGHT:
-        obstacle_y = -obstacle_height
-        obstacle_x = randint(road_left, road_right - obstacle_width)
+    obstacle_rect.y += obstacle_speed
+    if obstacle_rect.y > SCREEN_HEIGHT:
+        obstacle_rect.y = -obstacle_rect.height
+        obstacle_rect.x = randint(road_left, road_right - obstacle_rect.width)
     
     road_y += road_speed
     if road_y >= SCREEN_HEIGHT:
         road_y = 0
     
     pygame.draw.rect(screen, (50, 50, 50), (road_left, 0, road_width, SCREEN_HEIGHT))
-    pygame.draw.rect(screen, (50, 50, 50), (road_right, 0, road_width - SCREEN_HEIGHT, SCREEN_HEIGHT))
     pygame.draw.rect(screen, (255, 255, 255), (road_line_x, 0, road_line_width, SCREEN_HEIGHT))
-    
-    screen.blit(obstacle_img, (obstacle_x, obstacle_y))
+
+    screen.blit(obstacle_img, obstacle_rect)
     
     screen.blit(score_text, (10, 10))
     
@@ -79,19 +87,19 @@ while running:
     
     # Key handling 
     keys = pygame.key.get_pressed()
-    if keys[pygame.K_LEFT] and car_x > road_left + road_line_width:
-        car_x -= car_speed
-    if keys[pygame.K_RIGHT] and car_x < road_right - road_line_width - car_width:
-        car_x += car_speed
+    if keys[pygame.K_LEFT] and car_rect.left > road_left - road_line_width -5:
+        car_rect.x -= car_speed
+    if keys[pygame.K_RIGHT] and car_rect.right < road_right - road_line_width:
+        car_rect.x += car_speed
     
-    if (car_y < obstacle_y + obstacle_height and car_y + car_height > obstacle_y and car_x < obstacle_x + obstacle_width and car_x + car_width > obstacle_x):
+    if car_rect.colliderect(obstacle_rect):
         print("Crash!")
         running = False
 
     
     
     # draw car
-    screen.blit(car_img, (car_x, car_y))
+    screen.blit(car_img, car_rect)
 
     # update the screen
     pygame.display.flip()
